1127f294ecb77f0c82ad8b179f467702a0590a31
Abhishek Koirala <readeravskh@gmail.com>
2025-02-15 17:00:58 +0545

    Arithmetic Operators

---
+++ src/Tutorials2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val x = 15
+    val y = 7
+    val result = x + y
+    println(result)
+    println(x+y)
+    println(x-y)
+    println(x*y)
+    println(x/y)
+    println("the result of x+y is ${x+y}")
+}
