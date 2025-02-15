4632e8453fb7eaf6e0709027084fea61299a4026
Abhishek Koirala <readeravskh@gmail.com>
2025-02-15 17:18:28 +0545

    Strings

---
+++ src/Tutorials4.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("this is example text")
+    var x=10
+    println("this number is $x")
+    var message="this is message"
+    println(message)
+    println("Our string is: $message")
+
+    println("Our string is: ${message.uppercase()}")
+
+    val name="Abhishek"
+    println("Name in uppercase: ${name.uppercase()}")
+    println("Name in lowercase: ${name.lowercase()}")
+    println("Name in reverse: ${name.reversed()}")
+}
