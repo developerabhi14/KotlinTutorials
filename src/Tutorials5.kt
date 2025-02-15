69bc5f22a618e67f6cf54a07505c53dbf81da10d
Abhishek Koirala <readeravskh@gmail.com>
2025-02-15 19:10:06 +0545

    Comaprison operators

---
+++ src/Tutorials5.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val x = 4
+    val y = 5
+    println(y == x)
+    println(y<x)
+    println(y<=x)
+    println(y!=x)
+
+    val a=2
+    val b=5
+    println(x==a && y==b)
+    println(x==a || y==b)
+
+
+}
